dabdd8ad19ee2006517858e555cf0912a2a2d64a
mohammad horzadeh <hom77024@gmail.com>
2023-05-22 23:49:50 +0330

    image genertor finish

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-const apikey = "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";
+const apikey = "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";
 const submitIcon = document.querySelector("#submit-icon");
 const inputElement = document.querySelector("input");
 const imageSection = document.querySelector(".image-section");
 
 const getImage = async () => {
-    const option = {
+    const options = {
         method: "POST",
-        Headers:{
-            "Authorization": `bearer ${apikey}`,
+        headers:{
+            "Authorization": `Bearer ${apikey}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            prompt: inputElement.value,
-            n: 4,
-            size: "1024x1024"
+            "prompt": inputElement.value,
+            "n": 4,
+            "size": "1024x1024"
         })
     }
     try{
-        const response = await fetch("https://api.openai.com/v1/images/generations", option);
+        const response = await fetch("https://api.openai.com/v1/images/generations", options);
         const data = await response.json()
-        console.log(data);
         data.data.forEach(imageObject => {
             const imageContainer = document.createElement("div");
             imageContainer.classList.add("image-container")
